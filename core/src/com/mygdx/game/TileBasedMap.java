45f68b4cac064aa62de7a59157d3773a1e2824ca
jcaracciolo <jcaracciolo@itba.edu.ar>
2015-10-19 18:50:51 -0300

    ESTE FUNCIONA CON ECLIPSE

--- core/src/com/mygdx/game/TileBasedMap.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game;
-
-public interface TileBasedMap {
-	public int getWidthInTiles();
-    public int getHeightInTiles();	
-    public boolean blocked( int x, int y);	
-    public float getCost( int sx, int sy, int tx, int ty);
-    public void pathFinderVisited(int x, int y);
-}
